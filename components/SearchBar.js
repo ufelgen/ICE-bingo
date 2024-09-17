76da2a32594281cd2040a61bef48be02873305bc
Ulrike Felgenhauer <115288914+ufelgen@users.noreply.github.com>
2024-09-17 21:25:52 +0200

    Feature/logic (#2)
    * render trains in different way and try bingo logic
    
    * add button logic
    
    * Unterbrechung wegen Internetproblemen
    
    * fix isSeen logic
    
    * start bingo logic, determine current set
    
    * bingo is hard
    
    * check rows and columns for bingo
    
    * confetti for bingo
    
    * fix bingo logic
    
    * add diagonal logic, but it's only working in the first grid right now
    
    * deploy
    
    * toggle-Funktion auslagern
    
    * Bingo-Funktion auslagern und vorbereiten für 4er Grid
    
    * expand logic to 4 grid, alter confetti position
    
    * fix confetti
    
    * add search bar, move determineCurrentSetNumber function
    
    * add search and scroll logic
    
    * clean up code
    
    * fix search logic
    
    * fix layout
    
    * fix 4x4 layout and line break at /

---
+++ components/SearchBar.js
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+
+export default function SearchBar({
+  trainsArrayFor3by3,
+  trainsArrayFor4by4,
+  scrollToSection,
+}) {
+  function searchTrain(event) {
+    event.preventDefault();
+    const searchedTrain = event.target.elements.searchedTrain.value;
+    let currentTrain = {};
+    let currentTrainIndex = 0;
+    const currentTrainIn3 = trainsArrayFor3by3.find(
+      (train) => train.name == searchedTrain
+    );
+
+    const currentTrainIn4 = trainsArrayFor4by4.find(
+      (train) => train.name == searchedTrain
+    );
+
+    if (currentTrainIn3) {
+      currentTrain = currentTrainIn3;
+      currentTrainIndex = trainsArrayFor3by3.indexOf(currentTrain);
+      console.log("currentTrainIndex", currentTrainIndex);
+      //check which grid the currentTrain belongs to
+      const arrayIndex = Math.floor(currentTrainIndex / 9);
+      console.log("arrayIndex", arrayIndex);
+      scrollToSection(arrayIndex);
+    } else if (currentTrainIn4) {
+      currentTrain = currentTrainIn4;
+      currentTrainIndex = trainsArrayFor4by4.indexOf(currentTrain);
+      console.log("currentTrainIndex", currentTrainIndex);
+      //check which grid the currentTrain belongs to
+      //add the value 10 for 11 3x3 grids (indices 0-10)
+      const arrayIndex = Math.ceil(currentTrainIndex / 16) + 10;
+      console.log("arrayIndex", arrayIndex);
+      scrollToSection(arrayIndex);
+    } else if (!currentTrainIn3 && !currentTrainIn4) {
+      //hier kann noch ein Hinweis für den User hin
+      console.log("not found");
+    } else {
+      return;
+    }
+    event.target.reset();
+  }
+
+  return (
+    <StyledSearchBar onSubmit={() => searchTrain(event)}>
+      <input name="searchedTrain"></input>
+      <button type="submit">suchen</button>
+    </StyledSearchBar>
+  );
+}
+
+const StyledSearchBar = styled.form`
+  position: fixed;
+  top: 0;
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  height: 7vh;
+  width: 90%;
+  background-color: red;
+  margin: 0.5rem 2vw 0.5rem 2vw;
+  padding: 0.5rem;
+  border: 1px solid red;
+  border-radius: 5px;
+  gap: 0.5rem;
+
+  input {
+    border-radius: 5px;
+  }
+
+  button {
+    color: red;
+    background-color: lightgrey;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,163 @@
 import { shuffledTrains } from "../lib/shuffledTrains";
-import { Fragment } from "react";
+import { Fragment, useState, useRef } from "react";
 import styled from "styled-components";
+import useLocalStorageState from "use-local-storage-state";
+import dynamic from "next/dynamic";
+import { toggleSeen, splitUpInChunks } from "../helpers/bingoFunctions";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   //total 227 trains
   //use 3x3 and 4x4 layouts for bingo
   //divide array into 11x9 = 99 for 3x3
   //and 8x16 = 128 for 4x4
-  const trainsArrayFor3by3 = shuffledTrains.slice(0, 99);
-  const trainsArrayFor4by4 = shuffledTrains.slice(99, 227);
+  const trainsArrayFor3by3PRE = shuffledTrains.slice(0, 99);
+  const trainsArrayFor4by4PRE = shuffledTrains.slice(99, 227);
 
-  function splitUpInChunks(longArray, size) {
-    let chunks = [];
+  const [celebration, setCelebration] = useState(false);
+  const [trainsArrayFor3by3, setTrainsArrayFor3by3] = useLocalStorageState(
+    "trainsArrayFor3by3",
+    { defaultValue: trainsArrayFor3by3PRE }
+  );
 
-    longArray.forEach((item) => {
-      if (!chunks.length || chunks[chunks.length - 1].length == size)
-        chunks.push([]);
+  const [trainsArrayFor4by4, setTrainsArrayFor4by4] = useLocalStorageState(
+    "trainsArrayFor4by4",
+    { defaultValue: trainsArrayFor4by4PRE }
+  );
 
-      chunks[chunks.length - 1].push(item);
-    });
+  const arrayOfArraysForThreeGrid = splitUpInChunks(trainsArrayFor3by3, 3);
+  const finalArrayForThreeGrid = splitUpInChunks(arrayOfArraysForThreeGrid, 3);
+  const arrayOfArraysForFourGrid = splitUpInChunks(trainsArrayFor4by4, 4);
+  const finalArrayForFourGrid = splitUpInChunks(arrayOfArraysForFourGrid, 4);
 
-    return chunks;
+  const { height, width } = dynamic(() => import("../helpers/useWindowSize"), {
+    ssr: false,
+  });
+
+  const Confetti = dynamic(() => import("react-confetti"), {
+    ssr: false,
+  });
+
+  function handleCelebration() {
+    setCelebration(true);
+    setTimeout(handleConfettiStop, 5000);
+    //console.log(window.scrollY);
   }
 
-  const arrayOfArraysForThreeGrid = splitUpInChunks(trainsArrayFor3by3, 9);
-  const arrayOfArraysForFourGrid = splitUpInChunks(trainsArrayFor4by4, 16);
+  function handleConfettiStop() {
+    setCelebration(false);
+  }
+
+  const sectionRefs = useRef([]);
+
+  function scrollToSection(index) {
+    sectionRefs.current[index].scrollIntoView({ behavior: "smooth" });
+  }
 
   return (
-    <>
-      {arrayOfArraysForThreeGrid.map((arrayOfNineTrains) => (
-        <ThreeGrid key={arrayOfNineTrains[0].name}>
-          {arrayOfNineTrains.map((train) => (
-            <button key="train.id" type="button">
-              {train.name}
-            </button>
-          ))}
-        </ThreeGrid>
-      ))}
-      {arrayOfArraysForFourGrid.map((arrayOfSixteenTrains) => (
-        <FourGrid key={arrayOfSixteenTrains[0].name}>
-          {arrayOfSixteenTrains.map((train) => (
-            <button key="train.id" type="button">
-              {train.name}
-            </button>
-          ))}
-        </FourGrid>
-      ))}
-    </>
+    <Main>
+      <SearchBar
+        trainsArrayFor3by3={trainsArrayFor3by3}
+        trainsArrayFor4by4={trainsArrayFor4by4}
+        scrollToSection={scrollToSection}
+      />
+      {celebration && (
+        <>
+          <Confetti
+            height={10000}
+            width={width}
+            confettiSource={{
+              x: 0,
+              y: window.scrollY,
+              w: window.innerWidth,
+              h: 0,
+            }}
+            recycle={false}
+            numberOfPieces={7000}
+            initialVelocityY={window.innerHeight}
+          />
+        </>
+      )}
+      <GridContainer>
+        {finalArrayForThreeGrid.map((arrayOf3x3Trains, index) => (
+          <ThreeGrid
+            key={arrayOf3x3Trains[0][0].name}
+            ref={(el) => (sectionRefs.current[index] = el)}
+          >
+            {arrayOf3x3Trains.map((array) => (
+              <Fragment key={array[0].name}>
+                {array.map((train) => (
+                  <button
+                    key={train.id}
+                    type="button"
+                    className={train.isSeen ? "isSeen" : ""}
+                    id={train.id}
+                    onClick={() =>
+                      toggleSeen(
+                        train.id,
+                        setTrainsArrayFor3by3,
+                        trainsArrayFor3by3,
+                        3,
+                        handleCelebration
+                      )
+                    }
+                  >
+                    {train.name}
+                  </button>
+                ))}
+              </Fragment>
+            ))}
+          </ThreeGrid>
+        ))}
+        {finalArrayForFourGrid.map((arrayOf4x4Trains, index) => (
+          <FourGrid
+            key={arrayOf4x4Trains[0][0].name}
+            ref={(el) =>
+              (sectionRefs.current[index + finalArrayForThreeGrid.length] = el)
+            }
+          >
+            {arrayOf4x4Trains.map((array) => (
+              <Fragment key={array[0].name}>
+                {array.map((train) => (
+                  <button
+                    key={train.id}
+                    type="button"
+                    onClick={() =>
+                      toggleSeen(
+                        train.id,
+                        setTrainsArrayFor4by4,
+                        trainsArrayFor4by4,
+                        4,
+                        handleCelebration
+                      )
+                    }
+                    className={train.isSeen ? "isSeen" : ""}
+                  >
+                    {train.name}
+                  </button>
+                ))}
+              </Fragment>
+            ))}
+          </FourGrid>
+        ))}
+      </GridContainer>
+    </Main>
   );
 }
+
+const Main = styled.main`
+  position: relative;
+  background-color: purple;
+  width: 100vw;
+`;
+
+const GridContainer = styled.section`
+  position: fixed;
+  top: 10vh;
+  bottom: 2vh;
+  overflow-y: scroll;
+  width: 100%;
+`;
 
 const ThreeGrid = styled.section`
   display: grid;
@@ -60,17 +169,24 @@ const ThreeGrid = styled.section`
   border: 1px solid red;
   border-radius: 5px;
   background-color: lightgrey;
-  width: 100%;
-
+  width: 90vw;
+  height: 90vw;
   button {
     border: none;
     border-radius: 5px;
-    background-color: red;
-    color: white;
-    height: 30vw;
-    width: 30vw;
+    background-color: white;
+    color: red;
+    width: 25vw;
+    height: 25vw;
+    hyphens: auto;
     &:hover {
       color: black;
+    }
+
+    &.isSeen {
+      border: 1px solid white;
+      background-color: red;
+      color: white;
     }
   }
 `;
@@ -79,7 +195,7 @@ const FourGrid = styled(ThreeGrid)`
   grid-template: repeat(4, 1fr) / repeat(4, 1fr);
 
   button {
-    height: 22vw;
-    width: 22vw;
+    height: 20vw;
+    width: 20vw;
   }
 `;
